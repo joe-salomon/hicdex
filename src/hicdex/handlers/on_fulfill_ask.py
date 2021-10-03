a060053b8d4527f54846681a24f1b14a8643fd44
Joe Salomon <joseph.salomon@mindbodyonline.com>
2021-10-03 16:22:45 -0700

    except

--- src/hicdex/handlers/on_fulfill_ask.py
+++ src/hicdex/handlers/on_fulfill_ask.py
@@ -9,23 +9,26 @@ async def on_fulfill_ask(
     ctx: HandlerContext,
     fulfill_ask: Transaction[FulfillAskParameter, ObjktbidMarketplaceStorage],
 ) -> None:
-    ask = await models.Ask.filter(id=fulfill_ask.parameter.__root__).get().prefetch_related('creator')  # type: ignore
-    buyer, _ = await models.Holder.get_or_create(address=fulfill_ask.data.sender_address)
+    try:
+        ask = await models.Ask.filter(id=fulfill_ask.parameter.__root__).get().prefetch_related('creator')  # type: ignore
+        buyer, _ = await models.Holder.get_or_create(address=fulfill_ask.data.sender_address)
 
-    fulfilled_ask = models.FulfilledAsk(
-        ask=ask,
-        seller=ask.creator,
-        buyer=buyer,
-        objkt_id=ask.objkt_id,
-        amount=1,
-        level=fulfill_ask.data.level,
-        timestamp=fulfill_ask.data.timestamp,
-    )
-    await fulfilled_ask.save()
+        fulfilled_ask = models.FulfilledAsk(
+            ask=ask,
+            seller=ask.creator,
+            buyer=buyer,
+            objkt_id=ask.objkt_id,
+            amount=1,
+            level=fulfill_ask.data.level,
+            timestamp=fulfill_ask.data.timestamp,
+        )
+        await fulfilled_ask.save()
 
-    ask.amount_left -= 1  # type: ignore
-    if ask.amount_left == 0:
-        ask.status = models.AuctionStatus.CONCLUDED
-        ask.update_level = fulfill_ask.data.level  # type: ignore
-        ask.update_timestamp = fulfill_ask.data.timestamp  # type: ignore
-    await ask.save()
+        ask.amount_left -= 1  # type: ignore
+        if ask.amount_left == 0:
+            ask.status = models.AuctionStatus.CONCLUDED
+            ask.update_level = fulfill_ask.data.level  # type: ignore
+            ask.update_timestamp = fulfill_ask.data.timestamp  # type: ignore
+        await ask.save()
+    except Exception:
+        pass
