336253e18ace9e0476be5f490e25b3cc7dc47ebd
Joe Salomon <joseph.salomon@mindbodyonline.com>
2021-10-03 19:31:15 -0700

    try catch

--- src/hicdex/handlers/on_transfer.py
+++ src/hicdex/handlers/on_transfer.py
@@ -10,18 +10,21 @@ async def on_transfer(
     transfer: Transaction[TransferParameter, HenObjktsStorage],
 ) -> None:
     for t in transfer.parameter.__root__:
-        sender, _ = await models.Holder.get_or_create(address=t.from_)
-        for tx in t.txs:
-            receiver, _ = await models.Holder.get_or_create(address=tx.to_)
-            token = await models.Token.filter(id=int(tx.token_id)).get()
+        try:
+            sender, _ = await models.Holder.get_or_create(address=t.from_)
+            for tx in t.txs:
+                receiver, _ = await models.Holder.get_or_create(address=tx.to_)
+                token = await models.Token.filter(id=int(tx.token_id)).get()
 
-            sender_holding, _ = await models.TokenHolder.get_or_create(token=token, holder=sender)
-            sender_holding.quantity -= int(tx.amount)  # type: ignore
-            await sender_holding.save()
-            receiver_holding, _ = await models.TokenHolder.get_or_create(token=token, holder=receiver)
-            receiver_holding.quantity += int(tx.amount)  # type: ignore
-            await receiver_holding.save()
+                sender_holding, _ = await models.TokenHolder.get_or_create(token=token, holder=sender)
+                sender_holding.quantity -= int(tx.amount)  # type: ignore
+                await sender_holding.save()
+                receiver_holding, _ = await models.TokenHolder.get_or_create(token=token, holder=receiver)
+                receiver_holding.quantity += int(tx.amount)  # type: ignore
+                await receiver_holding.save()
 
-            if tx.to_ == 'tz1burnburnburnburnburnburnburjAYjjX':
-                token.supply -= int(tx.amount)  # type: ignore
-                await token.save()
+                if tx.to_ == 'tz1burnburnburnburnburnburnburjAYjjX':
+                    token.supply -= int(tx.amount)  # type: ignore
+                    await token.save()
+        except Exception:
+            pass
