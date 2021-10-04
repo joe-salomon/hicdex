f3d662c943846c65543d6cc322deffd50cab41de
Joe Salomon <joseph.salomon@mindbodyonline.com>
2021-10-04 10:31:00 -0700

    more exceptions

--- src/hicdex/handlers/on_conclude_english.py
+++ src/hicdex/handlers/on_conclude_english.py
@@ -9,10 +9,13 @@ async def on_conclude_english(
     ctx: HandlerContext,
     conclude_auction: Transaction[ConcludeAuctionParameter, ObjktbidEnglishStorage],
 ) -> None:
-    auction_model = await models.EnglishAuction.filter(id=int(conclude_auction.parameter.__root__)).get()
-    auction_model.status = models.AuctionStatus.CONCLUDED
+    try:
+        auction_model = await models.EnglishAuction.filter(id=int(conclude_auction.parameter.__root__)).get()
+        auction_model.status = models.AuctionStatus.CONCLUDED
 
-    auction_model.update_level = conclude_auction.data.level  # type: ignore
-    auction_model.update_timestamp = conclude_auction.data.timestamp  # type: ignore
+        auction_model.update_level = conclude_auction.data.level  # type: ignore
+        auction_model.update_timestamp = conclude_auction.data.timestamp  # type: ignore
 
-    await auction_model.save()
+        await auction_model.save()
+    except Exception:
+        pass
