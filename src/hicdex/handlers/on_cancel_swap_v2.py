05ba9385904208b13d26ebaecd1757ebe6b11bbb
Joe Salomon <joseph.salomon@mindbodyonline.com>
2021-10-04 12:46:02 -0700

    exception

--- src/hicdex/handlers/on_cancel_swap_v2.py
+++ src/hicdex/handlers/on_cancel_swap_v2.py
@@ -9,7 +9,10 @@ async def on_cancel_swap_v2(
     ctx: HandlerContext,
     cancel_swap: Transaction[CancelSwapParameter, HenSwapV2Storage],
 ) -> None:
-    swap = await models.Swap.filter(id=int(cancel_swap.parameter.__root__)).get()
-    swap.status = models.SwapStatus.CANCELED
-    swap.level = cancel_swap.data.level  # type: ignore
-    await swap.save()
+    try:
+        swap = await models.Swap.filter(id=int(cancel_swap.parameter.__root__)).get()
+        swap.status = models.SwapStatus.CANCELED
+        swap.level = cancel_swap.data.level  # type: ignore
+        await swap.save()
+    except Exception:
+        pass
